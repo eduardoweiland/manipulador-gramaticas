ba0131635bd25682c8c744488b063fd36ddb78dc
Eduardo Weiland <eduardo@eduardoweiland.info>
2015-04-06 15:44:22 -0300

    Testando requirejs-plugin-bower

---
+++ app/js/app.js
@@ -0,0 +1,11 @@
+define(['knockout'], function(ko) {
+
+	'use strict';
+
+	function App() {}
+
+	// Exporta o módulo
+	return App;
+
+});
+
---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+require.config({
+    bower: {
+        baseUrl: '../../bower_components',
+        root: '../../bower.json',
+        extensions: 'js|css',
+        ignore: 'requirejs',
+        auto: true,
+        deps: ['dependencies']
+    },
+    paths: {
+        'text': '../../bower_components/requirejs-text/text',
+        'json': '../../bower_components/requirejs-plugins/src/json',
+        'bower': '../../bower_components/requirejs-plugin-bower/src/bower'
+    }
+});
+
+define(['bower!bower.json'], function(bowerConfig) {
+    require(['app']);
+});
+

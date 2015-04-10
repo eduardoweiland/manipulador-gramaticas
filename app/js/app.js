27d2d41c0faecea0e9169e6952046bfc18ed8591
Eduardo Weiland <eduardo@eduardoweiland.info>
2015-04-10 13:20:09 -0300

    Removendo código específico para a versão antiga

--- app/js/app.js
+++ app/js/app.js
@@ -14,28 +14,8 @@ require(['knockout', 'jquery', 'grammar', 'bootstrap-tagsinput'], function(ko, $
                 $(this).parent().removeClass('focus');
             });
 
-        $('#clear').click(function() {
-            window.location.reload(true);
-        });
-
-        $('#run').click(function() {
-            var $run = $(this);
-            $run.addClass('running').prop('disabled', true);
-
-            // Começa a executar depois da animação do botão
-            setTimeout(function() {
-                setTimeout(function(){
-                    /* aqui vai terminar de executar */
-                    $run.removeClass('running').prop('disabled', false);
-                }, 5000);
-            }, 1000);
-        });
-
         ko.applyBindings(new Grammar());
 
-        setTimeout(function() {
-            $('.overlay').fadeOut().then().remove();
-        }, 1500);
+        $('.overlay').fadeOut();
     });
 });
-

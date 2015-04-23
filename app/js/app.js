a552f6ec0b1ce0f4bd9441229910285232afcdd1
Eduardo Weiland <eduardo@eduardoweiland.info>
2015-04-23 19:33:39 -0300

    Criando abas para separar as duas partes do trabalho

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,9 @@
 require(['knockout', 'jquery', 'grammar', 'ko-tagsinput', 'bootstrap-tagsinput'], function(ko, $, Grammar) {
     'use strict';
 
+    // Carrega os plugins JavaScript do Bootstrap (usado para as abas)
+    require(['../bower_components/bootstrap/dist/js/bootstrap.min.js']);
+
     $(function() {
         ko.applyBindings(new Grammar());
 
